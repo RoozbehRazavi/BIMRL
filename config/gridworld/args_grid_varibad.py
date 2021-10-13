f46696611b5544838a074abe149a64e32e07936b
RoozbehRazavi <reznov3395@gmail.com>
2021-10-13 11:00:46 +0330

    fix_rim

--- config/gridworld/args_grid_varibad.py
+++ config/gridworld/args_grid_varibad.py
@@ -260,11 +260,11 @@ def get_args(rest_args):
     parser.add_argument('--use_rim_level3', type=boolean_argument, default=True,
                         help='whatever create rim level3 (use for decode VAE terms) or not')
 
-    parser.add_argument('--rim_level1_hidden_size', type=int, default=32,
+    parser.add_argument('--rim_level1_hidden_size', type=int, default=8,
                         help='hidden size of level 1 rim (output of this level use for policy head)')
-    parser.add_argument('--rim_level2_hidden_size', type=int, default=32,
+    parser.add_argument('--rim_level2_hidden_size', type=int, default=8,
                         help='hidden size of level 1 rim (output of this level use for n step value prediction head)')
-    parser.add_argument('--rim_level3_hidden_size', type=int, default=32,
+    parser.add_argument('--rim_level3_hidden_size', type=int, default=8,
                         help='hidden size of level 3 rim (output of this level use decode VAE term)')
 
     parser.add_argument('--rim_level1_num_modules', type=int, default=4,
