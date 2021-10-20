08864c0cd70f27aee0f234cb5e945bc440a6d182
RoozbehRazavi <reznov3395@gmail.com>
2021-10-20 17:21:51 +0330

    meta_eval

--- brim_core/blocks.py
+++ brim_core/blocks.py
@@ -812,7 +812,7 @@ class Blocks(nn.Module):
                     extra_information['exploitation_policy_embedded_state'] = state_process(policy_state)
             else:
                 brim_output2 = torch.zeros(size=(*brim_hidden_state2.shape[:-1], self.rim_level1_output_dim), device=device)
-                extra_information['exploration_policy_embedded_state'] = state_process(policy_state)
+                extra_information['exploitation_policy_embedded_state'] = state_process(policy_state)
 
             if self.use_rim_level2:
                 level2_input = self.input_embedding_layer_level2[1](brim_input)
