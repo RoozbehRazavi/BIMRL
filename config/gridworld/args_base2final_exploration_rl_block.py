c5439423ad624d1a1f444cdcdf9b5a2e3a586b4d
RoozbehRazavi <reznov3395@gmail.com>
2021-11-09 09:29:44 +0330

    grad_stop

--- config/gridworld/args_base2final_exploration_rl_block.py
+++ config/gridworld/args_base2final_exploration_rl_block.py
@@ -52,7 +52,7 @@ def get_args(rest_args):
     parser.add_argument('--ppo_clip_param', type=float, default=0.05, help='clamp param')
 
     # other hyperparameters
-    parser.add_argument('--lr_policy', type=float, default=0.0007, help='learning rate (default: 7e-4)')
+    parser.add_argument('--lr_policy', type=float, default=0.0009, help='learning rate (default: 7e-4)')
     parser.add_argument('--num_processes', type=int, default=16,
                         help='how many training CPU processes / parallel environments to use (default: 16)')
     parser.add_argument('--policy_num_steps', type=int, default=400,
@@ -193,7 +193,7 @@ def get_args(rest_args):
                         help='detach output of task inference module when pass it to policy network')
     parser.add_argument('--vae_loss_throughout_vae_encoder_from_rim_level3', type=boolean_argument, default=False)
 
-    parser.add_argument('--n_step_value_prediction_coeff', type=float, default=1.0,
+    parser.add_argument('--n_step_value_prediction_coeff', type=float, default=0.0001,
                         help='weight for n step value prediction vs (VAE loss and RL loss)')
 
     parser.add_argument('--action_loss_coeff', type=float, default=1.0, help='weight for state loss')
@@ -281,9 +281,9 @@ def get_args(rest_args):
     parser.add_argument('--rim_level3_topk', type=int, default=3,
                         help='number of module in rim level 3 that can active in each time step')
 
-    parser.add_argument('--brim_layers_before_rim_level1', nargs='+', type=int, default=[16])
-    parser.add_argument('--brim_layers_before_rim_level2', nargs='+', type=int, default=[16])
-    parser.add_argument('--brim_layers_before_rim_level3', nargs='+', type=int, default=[16])
+    parser.add_argument('--brim_layers_before_rim_level1', nargs='+', type=int, default=[32, 16])
+    parser.add_argument('--brim_layers_before_rim_level2', nargs='+', type=int, default=[32, 16])
+    parser.add_argument('--brim_layers_before_rim_level3', nargs='+', type=int, default=[32, 16])
 
     parser.add_argument('--brim_layers_after_rim_level1', nargs='+', type=int, default=[8])
     parser.add_argument('--brim_layers_after_rim_level2', nargs='+', type=int, default=[8])
