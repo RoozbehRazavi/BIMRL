97e7a4ba886b723c31c2f0796f828376793f06ca
RoozbehRazavi <reznov3395@gmail.com>
2021-11-13 13:29:31 +0330

    change_in_envs_seed

--- utils/helpers.py
+++ utils/helpers.py
@@ -187,7 +187,7 @@ def compute_intrinsic_reward(rew_raw,
 
     intrinsic_rew_raw = (state_error * state_prediction_intrinsic_reward_coef * 0.2 + \
         action_error * action_prediction_intrinsic_reward_coef * 0.01 + \
-        reward_error * reward_prediction_intrinsic_reward_coef * 0.1) * annealing_tmp + \
+        reward_error * reward_prediction_intrinsic_reward_coef * 0.2) * annealing_tmp + \
         rew_raw * extrinsic_reward_intrinsic_reward_coef
     
     if isinstance(state_error, torch.Tensor):
