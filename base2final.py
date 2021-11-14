fd178ca0ad059690a8208aa0909d89b8701756a7
RoozbehRazavi <reznov3395@gmail.com>
2021-11-14 20:03:20 +0330

    change_in_envs_seed

--- base2final.py
+++ base2final.py
@@ -717,8 +717,8 @@ class Base2Final:
                                                                                value_decoder)
             losses = []
             for i in range(self.args.n_prediction + 1):
-                print(value_reconstruction_loss[i].shape)
                 losses.append(avg_loss(value_reconstruction_loss[i], self.args.vae_avg_elbo_terms, self.args.vae_avg_reconstruction_terms))
+                print(losses[i].shape)
             losses = torch.stack(losses)
             if self.args.vae_avg_n_step_prediction:
                 value_reconstruction_loss = losses.mean(dim=0)[0]
