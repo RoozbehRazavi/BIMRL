1040f41b15f99247e78c46bb83b3e580443a6735
RoozbehRazavi <reznov3395@gmail.com>
2021-11-18 11:42:19 +0330

    change_in_envs_seed

--- config/gridworld/args_base2final_exploration_memory.py
+++ config/gridworld/args_base2final_exploration_memory.py
@@ -304,9 +304,9 @@ def get_args(rest_args):
                         help='rim level 1 get information from task inference output')
     parser.add_argument('--rim_level2_condition_on_task_inference_latent', type=boolean_argument, default=True,
                         help='rim level 2 get information from task inference output')
-    parser.add_argument('--rim_top_down_level3_level2', type=boolean_argument, default=False,
+    parser.add_argument('--rim_top_down_level3_level2', type=boolean_argument, default=True,
                         help='rim level 2 get information from level 3')
-    parser.add_argument('--rim_top_down_level2_level1', type=boolean_argument, default=False,
+    parser.add_argument('--rim_top_down_level2_level1', type=boolean_argument, default=True,
                         help='rim level 1 get information from level 2')
     # memory
     parser.add_argument('--use_memory', type=boolean_argument, default=True,
