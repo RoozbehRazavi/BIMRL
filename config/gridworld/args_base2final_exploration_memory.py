451db539aa811e2ae16a634ca7c69a3dfdf100eb
RoozbehRazavi <reznov3395@gmail.com>
2021-12-07 17:49:27 +0330

    epi_reward

--- config/gridworld/args_base2final_exploration_memory.py
+++ config/gridworld/args_base2final_exploration_memory.py
@@ -335,7 +335,7 @@ def get_args(rest_args):
     parser.add_argument('--read_memory_to_value_layer', type=int, nargs='+', default=[])
     parser.add_argument('--read_memory_to_key_layer', type=int, nargs='+', default=[])
     parser.add_argument('--use_rpe', type=boolean_argument, default=False)
-    parser.add_argument('--hebb_learning_rate', type=float, default=0.0001)
+    parser.add_argument('--hebb_learning_rate', type=float, default=0.001)
     parser.add_argument('--reconstruction_memory_loss', type=boolean_argument, default=False)
     parser.add_argument('--reconstruction_memory_loss_coef', type=float, default=0.5,
                         help='RL vs Recons loss for memory training')
