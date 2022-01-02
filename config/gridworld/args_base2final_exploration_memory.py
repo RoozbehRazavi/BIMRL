4fb444a128b3e1c12d72716c02691cc01b3a9507
RoozbehRazavi <reznov3395@gmail.com>
2022-01-02 22:58:56 +0330

    increase_cap

--- config/gridworld/args_base2final_exploration_memory.py
+++ config/gridworld/args_base2final_exploration_memory.py
@@ -346,7 +346,7 @@ def get_args(rest_args):
     # vision core
     parser.add_argument('--use_stateful_vision_core', type=boolean_argument, default=True,
                         help='use attentional visual process unit')
-    parser.add_argument('--visual_attention_value_size', type=int, default=8)
+    parser.add_argument('--visual_attention_value_size', type=int, default=16)
     parser.add_argument('--visual_attention_key_size', type=int, default=4)
     parser.add_argument('--visual_attention_spatial', type=int, default=4)
     parser.add_argument('--visual_attention_num_queries', type=int, default=4)
