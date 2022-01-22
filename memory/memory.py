0ceab54a46ab4b0e89cd9b9789cb249555fe79b0
RoozbehRazavi <reznov3395@gmail.com>
2022-01-22 20:02:52 +0330

    regular_hebb

--- memory/memory.py
+++ memory/memory.py
@@ -183,12 +183,14 @@ class Hippocampus(nn.Module):
         q = self.rim_hidden_to_query(rim_hidden_state)
         k = torch.cat((epi_k, hebb_k), dim=1)
         v = torch.cat((epi_v, hebb_v), dim=1)
-        if torch.isnan(q).any():
-            print('############################Q is Nan')
-        if torch.isnan(k).any():
-            print('############################K is Nan')
-        if torch.isnan(v).any():
-            print('############################V is Nan')
+        if torch.isnan(epi_k).any():
+            print('############################epi_k is Nan')
+        if torch.isnan(hebb_k).any():
+            print('############################hebb_k is Nan')
+        if torch.isnan(epi_v).any():
+            print('############################epi_v is Nan')
+        if torch.isnan(hebb_v).any():
+            print('############################hebb_v is Nan')
         ans = self.read_mha(query=q.unsqueeze(1), key=k, value=v)[0].squeeze(1)
         return ans
 
