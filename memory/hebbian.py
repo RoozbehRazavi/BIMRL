af41ead81534514da1871c0d9600bcdd311100ad
RoozbehRazavi <reznov3395@gmail.com>
2022-01-22 20:10:59 +0330

    regular_hebb

--- memory/hebbian.py
+++ memory/hebbian.py
@@ -159,8 +159,11 @@ class Hebbian(nn.Module):
         else:
             raise NotImplementedError
         value = torch.bmm(query, w_assoc)
+        if torch.isnan(query).any():
+            print('!!!!!!!!!!!!!!!!!!!!!!!!!!')
         if torch.isnan(value).any():
             print('############################value is Nan')
+            print('############################w_assoc: ', w_assoc)
         value = value.reshape(batch_size, self.num_head*self.value_size)
         value = self.value_aggregator(value)
         k = self.read_memory_to_key(value)
